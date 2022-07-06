3a9fb41b852ce7787d6125bae2b00dcbfd690cdf
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-07-06 15:39:33 -0300

    fin

--- js/ejercicio9Tp1.js
+++ js/ejercicio9Tp1.js
@@ -1,7 +1,7 @@
 let frase = prompt('Ingrese una frase').toLowerCase()
 const vocales = [];
 for(let i = 0 ; i< frase.length; i++){
-    if(frase.charAt(i)==='a' || frase.charAt(i)==='e' ||frase.charAt(i)==='i' ||frase.charAt(i)==='o' ||frase.charAt(i)==='u'){
+    if(frase.charAt(i)==='a' || frase.charAt(i)==='e' ||frase.charAt(i)==='i' ||frase.charAt(i)==='o' ||frase.charAt(i)==='u' || frase.charAt(i)==='á' || frase.charAt(i)==='é' ||frase.charAt(i)==='í' ||frase.charAt(i)==='ó' ||frase.charAt(i)==='ú'){
         vocales.push(frase.charAt(i))
     }
 }
